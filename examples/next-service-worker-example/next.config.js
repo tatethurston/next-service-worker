a33fdd589a2d815ae7af641637ba7e2aaa6565ae
Tate Thurston <tatethurston@gmail.com>
2023-03-30 17:18:10 -0700

    more example updates

--- examples/next-service-worker-example/next.config.js
+++ examples/next-service-worker-example/next.config.js
@@ -1,6 +1,9 @@
+const { serviceWorker } = require("next-service-worker");
+const withServiceWorker = serviceWorker();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withServiceWorker(nextConfig);
